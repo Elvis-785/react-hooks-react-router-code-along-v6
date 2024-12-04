d56a7df2e2d7ee69e739c7e7a773bf9e7787f718
Elvis-785 <kiruielvis3358@gmail.com>
2024-12-04 16:02:38 +0300

    code along on react routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
+
+// import react-router functions
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router}/>)
+
+
